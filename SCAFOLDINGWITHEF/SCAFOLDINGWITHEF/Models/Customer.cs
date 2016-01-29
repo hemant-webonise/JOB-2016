db3f3550c74cf0a1e2905852ecc85664bf887ded
hemant-webonise <hemantbonde00@gmail.com>
2016-01-29 09:12:30 -0500

    Created Primary key for EF

--- SCAFOLDINGWITHEF/SCAFOLDINGWITHEF/Models/Customer.cs
+++ SCAFOLDINGWITHEF/SCAFOLDINGWITHEF/Models/Customer.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace SCAFOLDINGWITHEF.Models
 {
     public class Customer
     {
+        [Key]
         public int CustomerID { get; set; }
         public string CustomerName { get; set; }
     }
